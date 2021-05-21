01dac75032d6f88c89ae1fb50e50d6b24efe1673
Pol Feliu <plo.fc.123@gmail.com>
2021-05-21 12:22:06 +0200

    added support for nested structures

--- CCGstruct.py
+++ CCGstruct.py
@@ -1,11 +1,11 @@
 from typing import List
-
+from textwrap import indent
 from CCGtypes import *
 
 
 class StructMember:
 
-    def __init__(self, name, type, bitfield=None):
+    def __init__(self, name: str, type, bitfield=None):
         self.type = type
         self.name = name
         self.bitfield = bitfield
@@ -22,29 +22,34 @@ class StructMember:
 
 class Struct:
 
-    def __init__(self, tag):
+    def __init__(self, tag: str, members: List[StructMember]):
         self.tag = tag
+        self.members = members
 
     tag: str
     members: List[StructMember] = []
 
-    def declaration(self, name=None):
+    def declaration(self, name=None, semicolon=True):
         members = ""
         for member in self.members:
-            members += f"\t{member.declaration()}\r\n"
+            members += indent(member.declaration(), '\t') + "\r\n"
         return (
-            f"struct {self.tag} {{\r\n"
+            f"struct {self.tag}{{\r\n"
             f"{members}"
-            f"}};\r\n"
+            f"}}{name if name is not None else ''}{';' if semicolon else ''}"
         )
 
 
 if __name__ == "__main__":
 
-    ExampleStruct = Struct("examplestruct")
-    ExampleStruct.members.extend([
+    ExampleStruct = Struct("examplestruct_s", members=[
         StructMember("title", Int8(), bitfield=6),
-        StructMember("example", Array(type=Int8, length=3))
+        StructMember("example", Array(type=Int8, length=3)),
+        StructMember("Nestedstruct",
+             Struct("nestedstruct_s", members=[
+                StructMember("hello", Int8(), bitfield=6)
+             ])
+        )
     ])
 
     print(ExampleStruct.declaration())
