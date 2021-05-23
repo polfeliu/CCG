a8adf7175631cf64958777df5259764cbf9f170c
Pol Feliu <plo.fc.123@gmail.com>
2021-05-23 08:54:20 +0200

    function generation working except content, that needs the future statement class

--- CCGfunction.py
+++ CCGfunction.py
@@ -1,4 +1,5 @@
-from CCGtypes import GenericType
+from typing import List
+from CCGtypes import *
 
 class FunctionArgument:
 
@@ -11,17 +12,33 @@ class FunctionArgument:
 
 class Function:
 
-    def __init__(self, name, return_type, content=None):
+    def __init__(self, name, return_type=None, arguments: List[FunctionArgument] = None, content=None):
+        if arguments is None:
+            self.arguments = []
+        else:
+            self.arguments = arguments
+
         self.name = name
         self.return_type = return_type
         self.content = content
 
     name: str
-    return_type = None
+    return_type: GenericType = None
+    arguments: List[FunctionArgument]
     content = None  # TODO Change content for list of statements or something like that
 
     def declaration(self):
-        pass
+        argumentlist = ""
+        for argument in self.arguments:
+            argumentlist += f"{argument.type.typename} {argument.name}, "
+        argumentlist = argumentlist.rstrip(", ")
+
+        return (
+            f"{self.return_type.typename} {self.name}({argumentlist}){{\n"
+            f"{self.content if self.content is not None else ''}"
+            f"}}"
+        )
+
 
     def prototype(self):
         pass
@@ -30,4 +47,11 @@ if __name__ == "__main__":
     from CCGtypes import Uint32
     f = Function(
         name="examplefun",
-        return_type=Uint32
+        return_type=Uint32,
+        arguments=[
+            FunctionArgument(name="asdf", type=Uint32),
+            FunctionArgument(name="qwerw", type=Double)
+        ]
+        )
+
+    print(f.declaration())
