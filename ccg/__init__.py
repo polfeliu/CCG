acad48ddd6ebbfe8c1677f3e33b3b1d19a2cb109
Pol Feliu <pol.feliu@novanta.com>
2021-09-20 11:01:34 +0200

    import types

--- ccg/__init__.py
+++ ccg/__init__.py
@@ -1,5 +1,7 @@
 from .style import Style
 
+from .Ctypes import Cbool, Cfloat, Cdouble, Cint8, Cint16, Cint32, Cint64, Cuint8, Cuint16, Cuint32, Cuint64, \
+    std_types, CGenericType, CIntegerType
 from .Cvariable import CVariable
 from .Carray import CArray
 from .Cstruct import CStructDef
