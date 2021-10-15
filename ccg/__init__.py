2b774ee04726ef18d7faf1007ed2528e4d382e1d
Pol Feliu <feliupol@gmail.com>
2021-10-15 15:51:58 +0200

    modified lib

--- ccg/__init__.py
+++ ccg/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.0.1dev"
-
+print("experiment")
 from .style import Style
 
 from .Ctypes import Cbool, Cfloat, Cdouble, Cint8, Cint16, Cint32, Cint64, Cuint8, Cuint16, Cuint32, Cuint64, \
