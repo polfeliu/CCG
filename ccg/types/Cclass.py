028936eb6b9281cb28d3bad257b26731dfe6d22b
Pol Feliu <feliupol@gmail.com>
2021-11-05 18:08:58 +0100

    pep8

--- ccg/types/Cclass.py
+++ ccg/types/Cclass.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import TYPE_CHECKING, List, Union, Any, Optional
+from typing import TYPE_CHECKING, List, Union, Optional
 
 from .Cfunction import CFunction
 from ..Cstatement import CStatements
